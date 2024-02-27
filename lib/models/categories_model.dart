26edae95ca4434c07d877f469d0cc60e5b8487b5
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-02-27 11:04:56 +0530

    commit

--- lib/models/categories_model.dart
+++ lib/models/categories_model.dart
@@ -42,19 +42,22 @@ List<Category> categoriesData = [
       image: 'assets/images/pestControl.png', name: 'Pest Control Services'),
   Category(
       image: 'assets/images/cctvInstallation.png', name: 'CCTV Installation'),
-  // Category(image: 'assets/images/weldingMetalwork.png', name: 'Welding and Metalwork'),
-  // Category(image: 'assets/images/plasteringDrywall.png', name: 'Plastering and Drywall Repair'),
-  // Category(image: 'assets/images/homeCleaning.png', name: 'Home Cleaning Services'),
-  // Category(image: 'assets/images/locksmith.png', name: 'Locksmith Services'),
-  // Category(image: 'assets/images/securitySystemInstallation.png', name: 'Home Security System Installation'),
-  // Category(image: 'assets/images/ceilingFan.png', name: 'Ceiling Fan Installation/Repair'),
-  // Category(image: 'assets/images/drainageMaintenance.png', name: 'Drainage System Maintenance'),
-  // Category(image: 'assets/images/exteriorPainting.png', name: 'Exterior Painting'),
-  // Category(image: 'assets/images/deckPatio.png', name: 'Deck or Patio Construction/Repairs'),
+  Category(
+      image: 'assets/images/weldingMetalwork.png',
+      name: 'Welding and Metalwork'),
+  Category(
+      image: 'assets/images/plasteringDrywall.png',
+      name: 'Plastering and Drywall Repair'),
+  Category(
+      image: 'assets/images/homeCleaning.png', name: 'Home Cleaning Services'),
+  Category(image: 'assets/images/locksmith.png', name: 'Locksmith Services'),
+  Category(
+      image: 'assets/images/ceilingFan.png',
+      name: 'Ceiling and Exhaust Installation/Repair'),
+   Category(image: 'assets/images/drainageMaintenance.png', name: 'Drainage System Maintenance'),
   // Category(image: 'assets/images/fenceInstallation.png', name: 'Fence Installation/Repair'),
   // Category(image: 'assets/images/waterproofingBasements.png', name: 'Waterproofing Basements'),
   // Category(image: 'assets/images/lightFixture.png', name: 'Light Fixture Installation/Replacement'),
   // Category(image: 'assets/images/mirrorArtwork.png', name: 'Mirror and Artwork Hanging'),
   // Category(image: 'assets/images/powerWashing.png', name: 'Power Washing Services'),
-  // Category(image: 'assets/images/homeTheater.png', name: 'Home Theater System Setup'),
 ];
