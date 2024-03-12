438e1e464ec38857d8f81ac99f0abc290a586c4a
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-03-12 10:34:32 +0530

    12/March/2024

--- lib/screens/bookings_details_page.dart
+++ lib/screens/bookings_details_page.dart
@@ -5,8 +5,10 @@ import 'package:handyman/models/bookings_model.dart';
 import 'package:handyman/models/user.dart';
 import 'package:handyman/screens/home_screen.dart';
 import 'package:handyman/services/data/mybookings_service.dart';
+import 'package:handyman/utils/app_colors.dart';
 import 'package:handyman/widgets/customappbar.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:lottie/lottie.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -63,40 +65,55 @@ class _MyBookingsDetailsPageState extends ConsumerState<MyBookingsDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: CustomAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView(
               children: [
-                Text(
-                  'Booking Details',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Booking Details', style: myTextStylefontsize19Black),
+                    IconButton(
+                        onPressed: () {
+                          _buildBottom();
+                        },
+                        icon: const Icon(Icons.share))
+                  ],
                 ),
-                IconButton(
-                    onPressed: () {
-                      _buildBottom();
-                    },
-                    icon: const Icon(Icons.share))
+                const Divider(
+                  color: Colors.black,
+                  thickness: 0,
+                ),
+                const SizedBox(height: 20),
+                _buildDetailRow('Service Name', widget.booking.serviceName),
+                _buildDetailRow(
+                    'Provider Name', widget.booking.serviceProviderName),
+                _buildDetailRow('Service Subcategory',
+                    widget.booking.serviceSubCategoryName),
+                _buildDetailRow('Service Cost ₹', widget.booking.serviceCost),
+                _buildDetailRow(
+                    'Service Location', widget.booking.serviceLocation),
+                _buildDetailRow('Review', widget.booking.review),
+                _buildDetailRow('Distance', '${widget.booking.distance} km'),
+                _buildDetailRow(
+                    'Time of Booking', widget.booking.timeOfBooking.toString()),
+                SizedBox(height: 20),
+                const Divider(
+                  color: Colors.black,
+                  thickness: 0,
+                ),
+                SizedBox(height: 20),
+                _buildReviewSection(),
               ],
             ),
-            SizedBox(height: 20),
-            _buildDetailRow('Service Name', widget.booking.serviceName),
-            _buildDetailRow(
-                'Provider Name', widget.booking.serviceProviderName),
-            _buildDetailRow(
-                'Service Subcategory', widget.booking.serviceSubCategoryName),
-            _buildDetailRow('Service Cost', widget.booking.serviceCost),
-            _buildDetailRow('Service Location', widget.booking.serviceLocation),
-            _buildDetailRow('Review', widget.booking.review),
-            _buildDetailRow('Distance', '${widget.booking.distance} km'),
-            _buildDetailRow(
-                'Time of Booking', widget.booking.timeOfBooking.toString()),
-            SizedBox(height: 20),
-            _buildReviewSection(),
-          ],
+          ),
         ),
       ),
     );
@@ -127,11 +144,11 @@ class _MyBookingsDetailsPageState extends ConsumerState<MyBookingsDetailsPage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           'Add Your Review',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         TextField(
           controller: _reviewController,
           maxLines: 3,
@@ -141,25 +158,36 @@ class _MyBookingsDetailsPageState extends ConsumerState<MyBookingsDetailsPage> {
           ),
         ),
         SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () async {
-            String enteredReview = _reviewController.text;
-            if (enteredReview.isNotEmpty) {
-              try {
-                await ref
-                    .watch(myBookingsProvider)
-                    .addReview(widget.booking.bookingUID, enteredReview);
-                setState(() {
-                  addedReview = true;
-                  widget.booking.review = enteredReview;
-                });
-                print('Review submitted successfully');
-              } catch (e) {
-                print('Error submitting review: $e');
-              }
-            }
-          },
-          child: Text('Submit Review'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white),
+              onPressed: () async {
+                String enteredReview = _reviewController.text;
+                if (enteredReview.isNotEmpty) {
+                  try {
+                    await ref
+                        .watch(myBookingsProvider)
+                        .addReview(widget.booking.bookingUID, enteredReview);
+                    setState(() {
+                      addedReview = true;
+                      widget.booking.review = enteredReview;
+                    });
+                    _showDialog(context);
+                    _reviewController.clear();
+                  } catch (e) {
+                    SnackBar(
+                      content: Text("Error, Please try again"),
+                    );
+                  }
+                }
+              },
+              child: Text('Submit Review'),
+            ),
+          ],
         ),
       ],
     );
@@ -215,6 +243,28 @@ class _MyBookingsDetailsPageState extends ConsumerState<MyBookingsDetailsPage> {
               ),
             ],
           ),
+        );
+      },
+    );
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          titleTextStyle: myTextStylefontsize16Black,
+          title: Text('Thankyou for your feedback'),
+          content: Lottie.asset('assets/lottie/thankyou.json'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
         );
       },
     );
